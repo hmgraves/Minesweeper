54d74f657dcb8a84bb1a1e6d7fbb34d75c99dfd7
Hannah Graves <hannahmgraves@gmail.com>
2022-03-08 17:15:00 -0800

    updated mines placing

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Minesweeper
+
+## Introduction
+
+Minesweeper is the second game created by Hannah while in her first and second week at General Assembly. 
+
+### Screenshots
+
+### Technologies Used
+1. Javascript
+2. CSS
+3. HTML
+
+### Getting Started
+
+*INFO ABOUT HOW TO PLAY THE GAME*
+
+### Icebox
+1. Level difficulties (easy, medium, hard) - only building easy to start with. 
--- js/main.js
+++ js/main.js
@@ -1,23 +1,84 @@
-/*----- constants -----*/
+/*----- CONSTANTS -----*/
+const width = 9;
+const tbody = document.querySelector('tbody');
 
+/*----- app's state (VARIABLES) -----*/
+let score, winner;
+let mines = 10; 
+let flags = 0;
+let gameOver = false;
 
-/*----- app's state (variables) -----*/
+/*----- CACHED ELEMENTS  -----*/
 const tbodyEl = document.querySelector('tbody');
 const buttonEl = document.querySelector('button');
+function randomlyGenerateMineCoordinates() {
+    const mineCoordinates = Array.from({length: 10}, () => 'sq' + Math.floor(Math.random() * 81));
+    return mineCoordinates;
+} 
 
-/*----- cached element references -----*/
+let randomMines = randomlyGenerateMineCoordinates();
+console.log(randomMines)
 
-
-/*----- event listeners -----*/
-    /*----- game reset button -----*/
-
+/*----- EVENT LISTENERS -----*/
+/*----- game reset button -----*/
 buttonEl.addEventListener('click', function(evt) {
     console.log('reset clicked');
+    init();
 });
 
-    /*----- clicking on a sqaure -----*/
-tbodyEl.addEventListener('click', function() {
-    console.log('square clicked')
+/*----- left clicking on a square -----*/
+tbodyEl.addEventListener('click', function(playerLeftClick) {
+    console.log(document.getElementById(playerLeftClick.target.id).attributes.id.value);
+
+    let square = document.getElementById(playerLeftClick.target.id).attributes.id.value;
+    if (square = randomMines.find(mine => mine === square)) {
+        console.log('contains mine');
+        document.getElementById(square).classList.add('bombed');
+    } else {
+        console.log('contains no mine');
+        // square.classList.remove('flagged')  // removes flag
+    }
+    // console.log('square clicked')
 });
 
-/*----- functions -----*/
+/*----- right clicking on a square to add/remove flag-----*/
+tbodyEl.addEventListener('contextmenu', function(playerRightClick) {
+    const element = document.getElementById(playerRightClick.target.id);
+    if (element.classList != 'flagged') {
+        element.classList.add('flagged');
+        flags++;
+        document.getElementById('mines-remaining').innerHTML = 'Mines remaining: ' + (mines - flags); // updates mine count
+    } else { 
+        element.classList.remove('flagged')  // removes flag
+        flags--;
+        document.getElementById('mines-remaining').innerHTML = 'Mines remaining: ' + (mines - flags); // updates mine count
+    }
+    playerRightClick.preventDefault();
+    return false; /* Return false so that context menu does not pop up */
+}, false);
+
+
+/*----- FUNCTIONS -----*/
+
+
+let board = document.querySelectorAll('td');
+
+
+
+// let test = document.getElementById('sq1');
+// test.classList.add('bombed')
+// console.log(test)
+
+// let minedBoard = board.find(num=> randomlyGenerateMines.includes(num))
+
+
+
+
+
+
+
+
+
+
+
+
