1c909359b584603eb942723ee9f37aaeb455b693
Hannah Graves <hannahmgraves@gmail.com>
2022-03-09 11:23:12 -0800

    html updates and neighboring squares start

--- js/main.js
+++ js/main.js
@@ -12,12 +12,23 @@ let gameOver = false;
 const tbodyEl = document.querySelector('tbody');
 const buttonEl = document.querySelector('button');
 function randomlyGenerateMineCoordinates() {
-    const mineCoordinates = Array.from({length: 10}, () => 'sq' + Math.floor(Math.random() * 81));
+    const mineCoordinates = Array.from({length: 10}, () => Math.floor(Math.random() * 81));
     return mineCoordinates;
 } 
 
 let randomMines = randomlyGenerateMineCoordinates();
 console.log(randomMines)
+
+function checkWin() {
+    if (gameOver === false) {
+        console.log('continue game')
+    } else {
+        gameOver === true;
+        console.log('game over')
+    }
+}
+
+checkWin();
 
 /*----- EVENT LISTENERS -----*/
 /*----- game reset button -----*/
@@ -29,20 +40,32 @@ buttonEl.addEventListener('click', function(evt) {
 /*----- left clicking on a square -----*/
 tbodyEl.addEventListener('click', function(playerLeftClick) {
 
-    let square = document.getElementById(playerLeftClick.target.id).attributes.id.value;
-    if (square = randomMines.find(mine => mine === square)) {
-        console.log('contains mine');
-        document.getElementById(square).classList.add('bombed');
-    } else {
-        console.log('contains no mine');
-        // square.classList.remove('flagged')  // removes flag
+    let squareClicked = document.getElementById(playerLeftClick.target.id).attributes.id.value;
+    console.log(squareClicked)
+
+    if (randomMines.find(mine => mine === squareClicked)) { 
+        document.getElementById(squareClicked).classList.add('bombed'); // adds mine to screen
+        document.getElementById('mines-remaining').innerHTML = 'Game Over!';
+        gameOver = true;
+        checkWin();
+
+    } else if (squareClicked === randomMines.find(mine => {
+        console.log(mine);
+        console.log(mine + 1);
+        mine + 1
+        
+        })) {
+        document.getElementById(squareClicked).classList.add('one');
+        console.log(squareClicked);
+    
     }
     // console.log('square clicked')
 });
-
 /*----- right clicking on a square to add/remove flag-----*/
 tbodyEl.addEventListener('contextmenu', function(playerRightClick) {
+
     const element = document.getElementById(playerRightClick.target.id);
+
     if (element.classList != 'flagged') {
         element.classList.add('flagged');
         flags++;
@@ -52,8 +75,10 @@ tbodyEl.addEventListener('contextmenu', function(playerRightClick) {
         flags--;
         document.getElementById('mines-remaining').innerHTML = 'Mines remaining: ' + (mines - flags); // updates mine count
     }
+
     playerRightClick.preventDefault();
     return false; /* Return false so that context menu does not pop up */
+
 }, false);
 
 
@@ -70,3 +95,6 @@ tbodyEl.onclick = function(){
     }, 1000);
    this.onclick = null; // Stops timer from activating again. 
 };
+
+
+
